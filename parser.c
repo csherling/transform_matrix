440a20adba62d01a9c36d57dc0aa7294476ef955
csherling <csherling@stuy.edu>
2017-03-07 21:37:57 -0500

    Stuff doesn't work, idk why

--- parser.c
+++ parser.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 
 #include "ml6.h"
 #include "display.h"
@@ -22,26 +23,26 @@ The file follows the following format:
      Every command is a single character that takes up a line
      Any command that requires arguments must have those arguments in the second line.
      The commands are as follows:
-         line: add a line to the edge matrix - 
+         line: add a line to the edge matrix - (1)
 	    takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
-	 ident: set the transform matrix to the identity matrix - 
-	 scale: create a scale matrix, 
+	 ident: set the transform matrix to the identity matrix - (5)
+	 scale: create a scale matrix,  (2)
 	    then multiply the transform matrix by the scale matrix - 
 	    takes 3 arguments (sx, sy, sz)
-	 translate: create a translation matrix, 
+	 move: create a translation matrix, (3)
 	    then multiply the transform matrix by the translation matrix - 
 	    takes 3 arguments (tx, ty, tz)
-	 rotate: create an rotation matrix,
+	 rotate: create an rotation matrix, (4)
 	    then multiply the transform matrix by the rotation matrix -
 	    takes 2 arguments (axis, theta) axis should be x y or z
-	 apply: apply the current transformation matrix to the 
+	 apply: apply the current transformation matrix to the (6)
 	    edge matrix
-	 display: draw the lines of the edge matrix to the screen
+	 display: draw the lines of the edge matrix to the screen (7)
 	    display the screen
-	 save: draw the lines of the edge matrix to the screen
+	 save: draw the lines of the edge matrix to the screen (8)
 	    save the screen to a file -
 	    takes 1 argument (file name)
-	 quit: end parsing
+	 quit: end parsing (9)
 
 See the file script for an example of the file format
 
@@ -58,17 +59,104 @@ void parse_file ( char * filename,
                   screen s) {
 
   FILE *f;
-  char line[256];
+  char * line = (char *)malloc(256);
   clear_screen(s);
-
+  int com = 0;
+  color color;
+  color.red = MAX_COLOR;
+  color.green = MAX_COLOR;
+  color.blue = MAX_COLOR;
+  
   if ( strcmp(filename, "stdin") == 0 ) 
     f = stdin;
   else
     f = fopen(filename, "r");
   
   while ( fgets(line, 255, f) != NULL ) {
+    /* fgets(line, 255, f); */
     line[strlen(line)-1]='\0';
-    printf(":%s:\n",line);
+    printf("\n:%s:",line);
+    fflush(stdout);
+    /* if(com == -1){ */
+    /*   if(strcmp(line,"line") == 0) com = 1; */
+    /*   else if(strcmp(line,"scale") == 0) com = 2; */
+    /*   else if(strcmp(line,"move") == 0) com = 3; */
+    /*   else if(strcmp(line,"rotate") == 0) com = 4; */
+    /*   else if(strcmp(line,"ident") == 0) com = 5; */
+    /*   else if(strcmp(line,"apply") == 0) com = 6; */
+    /*   else if(strcmp(line,"display") == 0) com = 7; */
+    /*   else if(strcmp(line,"save") == 0) com = 8; */
+    /*   else if(strcmp(line,"quit") == 0) com = 9; */
+    /* } */
+    if(line[0]>=97 && line[0]<=119){
+      if(strncmp(line,"line",4) == 0) com = 1;
+      else if(strncmp(line,"scale",5) == 0) com = 2;
+      else if(strncmp(line,"move",4) == 0) com = 3;
+      else if(strncmp(line,"rotate",6) == 0) com = 4;
+      else if(strncmp(line,"ident",5) == 0) com = 5;
+      else if(strncmp(line,"apply",5) == 0) com = 6;
+      else if(strncmp(line,"display",7) == 0) com = 7;
+      else if(strncmp(line,"save",4) == 0) com = 8;
+      else if(strncmp(line,"quit",4) == 0) com = 9;
+      printf("%d alpha", com);
+    }
+    else{
+      if(com == 1){
+	printf("---1---");
+	add_edge(edges,atof(strsep(&line," ")),atof(strsep(&line," ")),atof(strsep(&line," ")),atof(strsep(&line," ")),atof(strsep(&line," ")),atof(line));
+	/* com = 0; */
+      }
+      else if(com == 2) {
+	printf("---2---");
+	make_scale(atof(strsep(&line," ")),atof(strsep(&line," ")),atof(line));
+	/* com = 0; */
+      }
+      else if(com == 3) {
+	printf("---3---");
+	make_translate(atof(strsep(&line," ")),atof(strsep(&line," ")),atof(line));
+	/* com = 0; */
+      }
+      else if(com == 4) {
+	printf("---4---");
+	char * dir = strsep(&line, " ");
+	if(strcmp(dir,"x") == 0) make_rotX(atof(line));
+	else if(strcmp(dir,"y") == 0) make_rotY(atof(line));
+	else if(strcmp(dir,"z") == 0) make_rotZ(atof(line));
+	matrix_mult(transform, edges);
+	/* com = 0; */
+      }
+      else if(com == 5) {
+	printf("---5---");
+	ident(transform);
+	/* com = -1; */
+      }
+      else if(com == 6) {
+	printf("---6---");
+	matrix_mult(transform, edges);
+	/* com = -1; */
+      }
+      else if(com == 7){
+	printf("---7---");
+	clear_screen(s);
+	draw_lines(edges, s, color);
+	display(s);
+	/* com = -1; */
+      }
+      else if(com == 8) {
+	printf("---8---");
+	clear_screen(s);
+	draw_lines(edges, s, color);
+	save_extension(s, "transform.png");
+	/* com = -1; */
+      }
+      else if(com == 9){
+	printf("---9---");
+	free(line);
+	break;
+      }
+      /* com = 0; */
+    }
+    
   }
 }
   
